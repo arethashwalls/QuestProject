eb444f0d68f4cbe2fdc489de8f065990b0addd75
ASH Walls <admin@arethawalls.com>
2019-03-28 13:56:58 -0700

    Deleted duplicate components

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,7 @@ import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
-// import Navbar from "./components/layout/Navbar";
-// import Landing from "./components/layout/Landing";
-// import Register from "./components/auth/Register";
-// import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
-// import Dashboard from "./components/dashboard/Dashboard";
 import Quest from './pages/Quest';
 import Welcome from './pages/Welcome';
 import Signup from './pages/Signup';
@@ -45,23 +40,7 @@ class App extends Component {
     theme: themes('redGreen')
   };
 
-// class App extends Component {
   render() {
-    // return (
-    //   <Provider store={store}>
-    //     <Router>
-    //       <div className="App">
-    //         <Navbar />
-    //         <Route exact path="/" component={Landing} />
-    //         <Route exact path="/register" component={Register} />
-    //         <Route exact path="/login" component={Login} />
-    //         <Switch>
-    //           <PrivateRoute exact path="/dashboard" component={Dashboard} />
-    //         </Switch>
-    //       </div>
-    //     </Router>
-    //   </Provider>
-    // );
     document.getElementById('root').style.backgroundColor = this.state.theme.lightBg.backgroundColor;
     return <Provider store={store}>
       <Router>
