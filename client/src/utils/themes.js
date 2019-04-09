0b8867b2801d4dbf1118545b71f78116c400a2a8
ASH Walls <admin@arethawalls.com>
2019-04-08 19:20:21 -0700

    Fixing get theme

--- client/src/utils/themes.js
+++ client/src/utils/themes.js
@@ -1,5 +1,4 @@
 const colorSchemes = {
-<<<<<<< HEAD
   login: {
     light: "#fefdd7"
   },
@@ -34,13 +33,6 @@ const getTheme = themeName => {
   return {
     backgrdPic: {
       imgFile: "images/background.jpg"
-=======
-    Warrior: {
-        dark: '#393F26',
-        light: '#818474',
-        highlight: '#EDDDD4',
-        splash: '#AD0101'
->>>>>>> 81d3f8d58ed4cbda79a529ce9e17193e529eaf12
     },
     darkBg: {
       backgroundColor: dark
