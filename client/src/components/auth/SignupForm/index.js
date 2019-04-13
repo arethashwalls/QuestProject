e3dacc3ea9c332314007296069f89e9f53b8fac2
Aretha S. H. Walls <43796460+arethashwalls@users.noreply.github.com>
2019-04-13 13:51:26 -0700

    Revert "Added pictures"

--- client/src/components/auth/SignupForm/index.js
+++ client/src/components/auth/SignupForm/index.js
@@ -10,7 +10,6 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
-import Image from 'react-bootstrap/Image';
 import "./style.css";
 
 class Register extends Component {
@@ -61,7 +60,7 @@ class Register extends Component {
       <Container fluid>
         <Row className='my-4'>
 
-          <Col xs={12} md={8} lg={6} className = "form">
+          <Col xs={12} lg={8} className = "form">
             <Form noValidate onSubmit={this.onSubmit}>
               <Form.Group>
                 <Form.Label>Name:</Form.Label>
@@ -148,9 +147,6 @@ class Register extends Component {
               <Button type='submit' className='float-left'>Your Quest Awaits...</Button>
 
             </Form>
-          </Col>
-          <Col xs={12} md={2} lg={6} className='text-right'>
-                <Image className='char-portrait' src={`images/${this.state.class || 'mage'}.png`} />
           </Col>
         </Row>
       </Container>
