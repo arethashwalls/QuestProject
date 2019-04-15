438aa54edfa16117f4f93cd00fbbfac3a8383cd1
ASH Walls <admin@arethawalls.com>
2019-04-15 16:41:45 -0700

    3rd times a charm?

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require('./config/passport')(passport);
 
 // Serve up static assets:
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
 // Database setup:
@@ -30,7 +30,7 @@ app.use('/api', apiRouter);
 
 // Send every other request to the React app:
 app.get('*', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.listen(PORT, () =>
