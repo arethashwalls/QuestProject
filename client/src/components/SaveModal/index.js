0ff925ed34a471f06072135095d972d85aaffc42
ASH Walls <admin@arethawalls.com>
2019-04-15 17:09:59 -0700

    Fixed save issue

--- client/src/components/SaveModal/index.js
+++ client/src/components/SaveModal/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 
 const SaveModal = (props) => {
     return <Modal show={props.show} onHide={props.close}>
-        <form>
+        <form onSubmit={() => { props.saveQuest(); props.close()}}>
             <Modal.Header>
                 <h3>Create adventure title</h3>
             </Modal.Header>
@@ -14,8 +14,19 @@ const SaveModal = (props) => {
                 </p>
             </Modal.Body>
             <Modal.Footer>
-                <button className="btn-cancel" onClick={props.close}>Cancel</button>
-                <button className="btn-continue" type="submit" onSubmit={() => { props.saveQuest(); props.close() }} >Save</button>
+                <button 
+                    className="btn-cancel" 
+                    onClick={props.close}
+                >
+                    Cancel
+                </button>
+                <button 
+                    className="btn-continue" 
+                    type="submit" 
+                    // onSubmit={() => { props.saveQuest(); props.close() }} 
+                >
+                    Save
+                </button>
             </Modal.Footer>
         </form>
     </Modal>
