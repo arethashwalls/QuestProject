fd1f7b60ec86f9b0cfbf049fd8194877a0033041
ASH Walls <admin@arethawalls.com>
2019-04-18 13:30:22 -0700

    Added alts and deleted comments

--- client/src/pages/NoMatch/index.js
+++ client/src/pages/NoMatch/index.js
@@ -4,18 +4,20 @@ const NoMatch = () => {
   return (
     <div>
       <div className="text-center">
-        <img src={process.env.PUBLIC_URL + "/images/quest.png"} />
+        <img 
+          src={process.env.PUBLIC_URL + "/images/quest.png"} 
+          alt='A Quest'
+        />
       </div>
       <br />
       <div className="nom">
         <h1 class="text-center">No Quests Here, Young Adventurer...</h1>
       </div>
-      {/* <div className="absolute"> */}
       <img
         src={process.env.PUBLIC_URL + "/images/404.png"}
         className="center"
+        alt='The skull of a doomed adventurer.'
       />
-      {/* </div> */}
       <div>
         <h1 className="text-center">Return to Your Quests...</h1>
       </div>
