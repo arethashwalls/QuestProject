f6a5e003339bd2d8bb7af21f62ee08cffb07a8cf
Carmina Garcia <carminagarcia97@gmail.com>
2019-04-17 23:56:38 -0700

    commiting

--- client/src/pages/NoMatch/index.js
+++ client/src/pages/NoMatch/index.js
@@ -4,12 +4,20 @@ const NoMatch = () => {
     return <div>
         <div class="text-center">
         <img src={process.env.PUBLIC_URL + "/images/quest.png"} />
-        <img src={process.env.PUBLIC_URL + "/images/dot.png"} />
+    
         </div>
         <br></br> 
         <div class="nom">
-            <h1 class="text-center">
+        <h1 class="text-center">
                 No Quests Here, Young Adventurer...
+            </h1>
+        </div>
+        <div class="absolute">
+        <img src={process.env.PUBLIC_URL + "/images/404.png"} />
+        </div>
+        <div>
+            <h1 class="text-center">
+                Return to Your Quests...
             </h1>
             </div>
     </div>
